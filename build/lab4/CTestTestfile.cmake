90fb0ebe2bb5edc4560fa3a74ea94dece3a2fbb3
Ari <aselfenbein@wpi.edu>
2017-04-08 12:50:03 -0700

    Add package for lab 4

---
+++ build/lab4/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/elfenari/RBE3002-PAC/src/lab4
+# Build directory: /home/elfenari/RBE3002-PAC/build/lab4
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
